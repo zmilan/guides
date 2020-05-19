20c17896c9057af134036e524794a9e9b3dcd868
Alec <hello@itsalec.co.uk>
2020-05-19 08:20:23 +0100

    Init

---
+++ navigation.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'Getting Started' => [
+        'children' => [
+            'API' => [
+                'children' => [
+                    'Installation' => 'docs/api/installation',
+                    'Configuration' => 'docs/api/configuration',
+                    'Order Lifecycle' => 'docs/api/order-lifecycle',
+                ]
+            ],
+            'Admin Hub' => [
+                'children' => [
+                    'Installation' => 'docs/hub/installation/admin-hub'
+                ]
+            ]
+        ],
+    ],
+    'Extending the API' => [
+        'children' => [
+            'Custom Payment Providers' => 'docs/api/extending/payment-providers'
+        ]
+    ]
+];
