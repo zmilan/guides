25b91e870ed603fd3f7f853cb0f11271a6b26fa7
Alec <hello@itsalec.co.uk>
2020-05-20 09:38:26 +0100

    Add assets

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,19 @@
 module.exports = {
   title: 'GetCandy Guides',
-  description: 'Just playing around',
+  description: 'Guides on how to install and use the GetCandy API & Admin hub',
+  head: [
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/icons/apple-touch-icon.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/icons/favicon-32x32.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/icons/favicon-16x16.png"}],
+    ['link', { rel: "manifest", href: "/assets/icons/site.webmanifest"}],
+    ['link', { rel: "mask-icon", href: "/assets/icons/safari-pinned-tab.svg", color: "#3a0839"}],
+    ['link', { rel: "shortcut icon", href: "/assets/icons/favicon.ico"}],
+    ['meta', { name: "msapplication-TileColor", content: "#3a0839"}],
+    ['meta', { name: "msapplication-config", content: "/assets/icons/browserconfig.xml"}],
+    ['meta', { name: "theme-color", content: "#ffffff"}],
+  ],
   themeConfig: {
+    logo: '/assets/img/logo.png',
     nav: [
       { text: 'Home', link: '/' },
       { text: 'GetCandy Website', link: 'https://getcandy.io' },
