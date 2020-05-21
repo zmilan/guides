7f0115e36785b888940dee4b1c102da1c5b43ee2
Alec <hello@itsalec.co.uk>
2020-05-21 13:06:39 +0100

    Add php client link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -97,6 +97,7 @@ module.exports = {
         collapsable: false, // optional, defaults to true
         sidebarDepth: 1,    // optional, defaults to 1
         children: [
+          ['https://github.com/getcandy/php-client', 'PHP'],
           ['https://github.com/getcandy/js-client', 'Javascript'],
           ['https://github.com/getcandy/js-client-nuxt', 'Javascript Nuxt Module'],
         ]
