292a7ca3ba079c1041e7d04b6988a806d82a8332
Glenn Jacobs <glenn@DESKTOP-8KLU3GD.localdomain>
2020-06-11 17:25:19 +0100

    Updating Favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400;500;700&family=Patua+One&family=Source+Code+Pro&display=swap' }
     ]
   },
